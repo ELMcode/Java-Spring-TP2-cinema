6ff5a9217edb3020534b7c2a2b87e55d633215d3
amine.elalame <amine.elalame@pingflow.fr>
2024-01-31 16:23:58 +0100

    feat(cda/cinemacda4/controller) : Add functions to return actors & film director from movies ID's :sparkles:

--- src/main/java/fr/cda/cinemacda4/controller/FilmController.java
+++ src/main/java/fr/cda/cinemacda4/controller/FilmController.java
@@ -1,48 +1,75 @@
 package fr.cda.cinemacda4.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fr.cda.cinemacda4.dto.*;
 import fr.cda.cinemacda4.entity.Film;
+import fr.cda.cinemacda4.entity.Realisateur;
 import fr.cda.cinemacda4.service.FilmService;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/films")
 public class FilmController {
     private final FilmService filmService;
 
-    public FilmController(FilmService filmService) {
+    private final ObjectMapper objectMapper;
+
+    public FilmController(
+            FilmService filmService,
+            ObjectMapper objectMapper
+    ) {
         this.filmService = filmService;
+        this.objectMapper = objectMapper;
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Film> findAll() {
-        return filmService.findAll();
+    @GetMapping
+    public List<FilmReduitDto> findAll() {
+        return filmService.findAll().stream().map(
+                film -> objectMapper.convertValue(film, FilmReduitDto.class)
+        ).toList();
     }
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping
     public Film save(@RequestBody Film film) {
         return filmService.save(film);
     }
 
-    @GetMapping(path ="/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Film findById(@PathVariable Integer id) {
-        return filmService.findById(id);
+    @GetMapping("/{id}") // /films/1
+    public FilmCompletDto findById(@PathVariable Integer id) {
+        Film film = filmService.findById(id);
+        return objectMapper.convertValue(film, FilmCompletDto.class);
     }
 
-    @DeleteMapping(path ="/{id}")
+    @DeleteMapping("/{id}")
     public void deleteById(@PathVariable Integer id) {
         filmService.deleteById(id);
     }
 
-    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping
     public Film update(@RequestBody Film film) {
         return filmService.update(film);
     }
 
-    @GetMapping(path = "/search")
+    @GetMapping("/search") // /film/search?titre=toto
     public Film findByTitre(@RequestParam String titre) {
         return filmService.findByTitre(titre);
+    }
+
+    @GetMapping("/{id}/acteurs")
+    public List<ActeurSansIdDto> getActeursByFilmId(@PathVariable Integer id) {
+        Film film = filmService.findById(id);
+        return film.getActeurs().stream()
+                .map(acteur -> objectMapper.convertValue(acteur, ActeurSansIdDto.class))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}/realisateur")
+    public RealisateurSansFilmsDto getRealisateurByFilmId(@PathVariable Integer id) {
+        Film film = filmService.findById(id);
+        Realisateur realisateur = film.getRealisateur();
+        return objectMapper.convertValue(realisateur, RealisateurSansFilmsDto.class);
     }
 }
