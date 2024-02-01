789711aaa91c13edb42aae88eb8031779f076df9
amine.elalame <elm.dev.code@gmail.com>
2024-02-01 14:52:54 +0100

    feat(cinemacda4/entity): TP4 - add 'Ticket' entity

---
+++ src/main/java/fr/cda/cinemacda4/entity/Ticket.java
@@ -0,0 +1,26 @@
+package fr.cda.cinemacda4.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "ticket")
+public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "seance_id", referencedColumnName = "id")
+    private Seance seance;
+
+    private String nomClient;
+
+    private Integer nbPlaces;
+}
