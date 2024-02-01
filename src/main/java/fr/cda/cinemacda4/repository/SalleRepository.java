2d9d8ad87d3cb6e47a7fcff7fc681a7d9f732281
amine.elalame <elm.dev.code@gmail.com>
2024-02-01 15:44:50 +0100

    feat(cinemacda4/repository): TP4 - Add findByNom method to SalleRepository interface :sparkles:

--- src/main/java/fr/cda/cinemacda4/repository/SalleRepository.java
+++ src/main/java/fr/cda/cinemacda4/repository/SalleRepository.java
@@ -1,7 +1,11 @@
 package fr.cda.cinemacda4.repository;
 
+import fr.cda.cinemacda4.entity.Film;
 import fr.cda.cinemacda4.entity.Salle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SalleRepository extends JpaRepository<Salle, Integer> {
+    Optional<Salle> findByNom(String nom);
 }
