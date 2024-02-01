df55fb563c05977e6d5287a8347ea1583d468f81
amine.elalame <elm.dev.code@gmail.com>
2024-02-01 15:04:52 +0100

    feat(cinemacda4/repository): TP4 - add Ticket repository :sparkles:

---
+++ src/main/java/fr/cda/cinemacda4/repository/TicketRepository.java
@@ -0,0 +1,7 @@
+package fr.cda.cinemacda4.repository;
+
+import fr.cda.cinemacda4.entity.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TicketRepository extends JpaRepository<Ticket, Integer> {
+}
