6b077c5f9f680fed8278cccc46e46c1cd9621189
amine.elalame <elm.dev.code@gmail.com>
2024-02-02 16:39:52 +0100

    feat(cinemacda4/repository): TP4 - Update Ticket repository . Project still WIP :construction:

--- src/main/java/fr/cda/cinemacda4/repository/TicketRepository.java
+++ src/main/java/fr/cda/cinemacda4/repository/TicketRepository.java
@@ -3,5 +3,8 @@ package fr.cda.cinemacda4.repository;
 import fr.cda.cinemacda4.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TicketRepository extends JpaRepository<Ticket, Integer> {
+    List<Ticket> findBySeanceIdAndIsReservedTrue(Integer seanceId);
 }
